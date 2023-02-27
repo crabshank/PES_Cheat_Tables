3431423051be8fa3ffd0f7beced3e5cd7c7045a4
crabshank <44329154+crabshank@users.noreply.github.com>
2023-02-27 06:55:28 +0000

    Add files via upload

---
+++ Tests/gk_o_READ_opcodes.asm
@@ -0,0 +1,14 @@
+-- Most to least accessed:
+base+78 (default = 0) (#148) cmp [rcx+08],dl 
+base+79 (default = 0) (#149) mov [rcx+09],al 
+base+7C (default = 0) (#152) mov eax,[rcx+0C] 
+base+58 (default = 4) (#116) subss xmm8,[rax+78] 
+base+54
+base (default = 0.23) (#28) movss xmm6,[rdi+20] 
+base+8
+base+10
+base-4
+base+4 (default = 45) (#32) movss xmm6,[rax+24] 
+base+38 (default = 10) (#84) movss xmm1,[rax+58] 
+base+3C (default = 50) (#88) movss xmm8,[rax+5C] 
+base-10 (default = 60) (#12) movss xmm0,[rax+10] 
